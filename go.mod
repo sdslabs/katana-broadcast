895b0fd67058cfebc272637b65483b0e99020c16
Mayank Mittal <mmittal123456@gmail.com>
2022-02-11 03:02:50 +0530

    add execute script

--- go.mod
+++ go.mod
@@ -1,20 +1,16 @@
-module github.com/sdslabs/katanad
+module github.com/sdslabs/katana-broadcast
 
 go 1.17
 
 require (
-	github.com/sdslabs/katanad/proto v0.0.0
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.27.1
 )
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220204002441-d6cc3cc0770e // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
-
-replace github.com/sdslabs/katanad/protobuf v0.0.0 => ./protobuf
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"archive/zip"
 	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"os/exec"
+	"path/filepath"
 
-	pb "github.com/sdslabs/katanad/protobuf"
+	pb "github.com/sdslabs/katana-broadcast/protobuf"
 
 	"google.golang.org/grpc"
 )
@@ -57,7 +60,8 @@ func (s *server) UploadFile(stream pb.FileUploadService_UploadFileServer) error 
 		log.Println("Error: ", err)
 		return err
 	}
-	f, err := os.Create(fmt.Sprintf("%s.zip", fileName))
+	artifactPath := fileName
+	f, err := os.Create(artifactPath)
 	if err != nil {
 		return err
 	}
@@ -65,11 +69,85 @@ func (s *server) UploadFile(stream pb.FileUploadService_UploadFileServer) error 
 	if err != nil {
 		return err
 	}
+	go handleFile(artifactPath, chalName)
 	return nil
 }
 
+func handleFile(artifactPath string, chalName string) {
+	challengeRoot := filepath.Join(os.Getenv("CHALLENGE_DIR"), chalName)
+	zipFile, err := zip.OpenReader(artifactPath)
+	defer zipFile.Close()
+	var filenames []string
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	for _, f := range zipFile.File {
+		fpath := filepath.Join(challengeRoot, f.Name)
+		filenames = append(filenames, fpath)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			log.Fatalln("Error: ", err)
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			log.Fatalln("Error: ", err)
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			log.Fatalln("Error: ", err)
+		}
+
+		_, err = io.Copy(outFile, rc)
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			log.Fatalln("Error: ", err)
+		}
+	}
+	err = os.Remove(artifactPath)
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	initFile := os.Getenv("INIT_FILE")
+	if !checkFile(filenames, initFile, filepath.Join(challengeRoot)) {
+		log.Fatalln("Error: ", os.ErrNotExist)
+	}
+	outlog, err := os.Create(filepath.Join(challengeRoot, "out.log"))
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	errlog, err := os.Create(filepath.Join(challengeRoot, "out.log"))
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	err = os.Chmod(filepath.Join(challengeRoot, initFile), 0o755)
+	cmd := exec.Command("bash", filepath.Join(challengeRoot, initFile))
+	cmd.Stdout = outlog
+	cmd.Stderr = errlog
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+}
+
+func checkFile(filenames []string, file string, path string) bool {
+	for _, file := range filenames {
+		log.Println(file)
+		if file == filepath.Join(path, os.Getenv("INIT_FILE")) {
+			return true
+		}
+	}
+	return false
+}
 func setupServer() error {
-	lis, err := net.Listen("tcp", ":5050")
+	port := os.Getenv("DAEMON_PORT")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
 		return err
 	}
